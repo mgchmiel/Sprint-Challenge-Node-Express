c3a8174afbcc0c5426ebd1b873d727cc56ceca3e
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-05-04 00:53:51 -0400

    Created server.js and installed all dependencies

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('cors');
+const actionRouter = require('./actions/actionRouter.js');
+const projectRouter = require('./projects/projectRouter.js');
+const server = express();
+const port = 5000;
+
+const logger = (req, res, next) => {
+    console.log(`Requested URL: ${req.url}`);
+    console.log(`Request Info: ${req.body}`);
+    next();
+
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+
+server.listen(port, () => console.log(`API Is Running Running on Port:${port}`));
